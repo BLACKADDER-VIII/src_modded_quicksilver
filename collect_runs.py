dfb1e6c4cf02665c9e397c246be9874da36a0588
BLACKADDER-VIII <dhroov.1a@gmail.com>
2026-09-21 23:03:02 +0000

    Updated run collector

--- collect_runs.py
+++ collect_runs.py
@@ -5,15 +5,25 @@ HERE = Path.cwd()
 STORE_DIR = HERE/"runs_data"
 os.system(f"rm -rf {STORE_DIR}")  # Cleanup
 
-NUM_RUNS = int(sys.argv[1]) if len(sys.argv) > 1 else 10
+if len(sys.argv) < 2:
+    print("Need Ranks size")
+    exit()
+NUM_RANKS = int(sys.argv[1])
+NUM_RUNS = int(sys.argv[2]) if len(sys.argv) > 2 else 10
 
 for r in range(NUM_RUNS):
     run_dir = STORE_DIR/f"run_{r}"
     os.makedirs(run_dir)
     subprocess.run(["cp", HERE/"qs_large.sh", run_dir])
     subprocess.run(["cp", HERE/"default_glibc.json", run_dir])
-    subprocess.run(["bash", "qs_large.sh"], cwd=run_dir)
-    subprocess.run(["python", HERE/"process_ooo.py", str(run_dir)], cwd=run_dir)
+    if NUM_RANKS == 32:
+        subprocess.run(["bash", "qs_32.sh"], cwd=run_dir)
+    elif NUM_RANKS == 64:
+        subprocess.run(["bash", "qs_64.sh"], cwd=run_dir)
+    else:
+        print("Need ranks to be 32 or 64")
+        exit()
+    subprocess.run(["python", "/home/exouser/mcb_test_app/process_ooo.py", str(run_dir)], cwd=run_dir)
     print(f"Finished run {r}...")
 
 print("Finished all runs")
